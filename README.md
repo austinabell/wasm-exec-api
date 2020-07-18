7592abd0ef2890764795882a457e0ccaecc44ad8
austinabell <austinabell8@gmail.com>
2020-07-18 10:50:02 -0400

    Switch to dynamic functions and setup logging

--- README.md
+++ README.md
@@ -2,14 +2,30 @@
 
 **This is wip and just as poc/fun for now** 
 
-Server that sets up a wasm runtime on request with hex dump of wasm binary and function call and executes
+Server that sets up a wasm runtime on request with hex dump of wasm binary, function name to call, and optionally parameters for the function call and executes.
 
-Call wasm code with hex dump of wasm file and function name. Params and return type are optional.
-Example:
+### Example request
 
 ```
-curl -X POST --data '{"wasm_hex": "0061736d0100000001060160017f017f030201000707010372756e00000a0601040020000b", "function_name": "run", "params": {"i32": 2}, "return_type": "i32"}' -H "Content-Type: application/json" http://localhost:8080/
+curl -X POST --data '{"wasm_hex": "0061736d0100000001060160017f017f030201000707010372756e00000a0601040020000b", "function_name": "run", "params": [{"I32": 2}]}' -H "Content-Type: application/json" http://localhost:8080/
 ```
+
+### All wasm param types:
+```
+    {"I32": 0}
+    {"I64": 0}
+    {"F32": 0.0}
+    {"F64": 0.0}
+    {"V64": 0}
+```
+
+## Next steps
+
+- [x] Remote code execution on server
+- [x] Dynamic function calls to not have to specify return type and manually handle cases
+- [ ] Wasm code inspection to be able to infer the params types to not have to specify the enum of possible values (example: `2` instead of `{"I32": 2}`) for each value and give better error returns
+- [ ] Functionality to set shared memory of runtime environment to be able to be used in func
+- [ ] Implement way to setup host functions to access during execution
 
 ## Generating hex dump of wasm file
 
--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,36 @@
 use actix_web::{error, web, App, HttpServer, Result};
+use log::LevelFilter;
 use serde::Deserialize;
 use std::string::ToString;
-use wasmer_runtime::{imports, instantiate, Func, Instance};
-// use wasmer_runtime_core::types::WasmExternType;
+use wasmer_runtime::{imports, instantiate, DynFunc, Instance, Value as WasmValue};
 
-#[derive(Deserialize, Debug)]
-#[serde(rename_all = "snake_case")]
-enum WasmParam {
-    I32(i32),
-    I64(i64),
-    U32(u32),
-    U64(u64),
-    F32(f32),
-    F64(f64),
-    TupleTwo(Box<WasmParam>, Box<WasmParam>),
-    TupleThree(Box<WasmParam>, Box<WasmParam>, Box<WasmParam>),
+fn setup_logger() {
+    let mut logger_builder = pretty_env_logger::formatted_timed_builder();
+    if let Ok(s) = ::std::env::var("RUST_LOG") {
+        logger_builder.parse_filters(&s);
+    } else {
+        logger_builder.filter(None, LevelFilter::Info);
+    }
+    let logger = logger_builder.build();
+    async_log::Logger::wrap(logger, || 12)
+        .start(log::LevelFilter::Trace)
+        .unwrap();
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct RequestPayload {
     wasm_hex: String,
     function_name: String,
-    params: Option<WasmParam>,
-    return_type: Option<String>,
+    #[serde(default)]
+    params: Vec<WasmValue>,
 }
 
-fn call_fn(
-    instance: &Instance,
-    fn_name: &str,
-    params: Option<WasmParam>,
-    return_type: &str,
-) -> Result<String, String> {
-    use WasmParam::*;
-    match (&params, return_type) {
-        (None, "i32") => {
-            let function: Func<(), i32> =
-                instance.exports.get(&fn_name).map_err(|e| e.to_string())?;
-            function
-                .call()
-                .map(|r| r.to_string())
-                .map_err(|e| e.to_string())
-        }
-        (Some(I32(i)), "i32") => {
-            let function: Func<i32, i32> =
-                instance.exports.get(&fn_name).map_err(|e| e.to_string())?;
-
-            function
-                .call(*i)
-                .map(|r| r.to_string())
-                .map_err(|e| e.to_string())
-        }
-        _ => Err(format!(
-            "params: {:?}, return_type: {:?} not supported",
-            params, return_type
-        )),
-    }
+fn call_fn(instance: &Instance, fn_name: &str, params: Vec<WasmValue>) -> Result<String, String> {
+    let function: DynFunc = instance.exports.get(&fn_name).map_err(|e| e.to_string())?;
+    function
+        .call(&params)
+        .map(|r| format!("{:?}", r))
+        .map_err(|e| e.to_string())
 }
 
 fn call_wasm(
@@ -62,7 +38,6 @@ fn call_wasm(
         wasm_hex,
         function_name,
         params,
-        return_type,
     }: RequestPayload,
 ) -> Result<String, String> {
     let wasm_bytes = hex::decode(&wasm_hex).map_err(|e| e.to_string())?;
@@ -70,13 +45,7 @@ fn call_wasm(
     // Instantiate the wasm runtime
     let instance = instantiate(&wasm_bytes, &imports! {}).map_err(|e| e.to_string())?;
 
-    let return_type = if let Some(ty) = return_type {
-        ty
-    } else {
-        return Ok("".to_owned());
-    };
-
-    let res = call_fn(&instance, &function_name, params, &return_type)?;
+    let res = call_fn(&instance, &function_name, params)?;
 
     // Convert result into string to return
     Ok(res.to_string())
@@ -88,8 +57,35 @@ async fn index(req: web::Json<RequestPayload>) -> Result<String> {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    setup_logger();
+
     HttpServer::new(|| App::new().route("/", web::post().to(index)))
         .bind("127.0.0.1:8080")?
         .run()
         .await
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn req_serialization() {
+        let req_payload = r#"{
+            "wasm_hex": "0061736d0100000001060160017f017f030201000707010372756e00000a0601040020000b",
+            "function_name": "run",
+            "params": [{"I32": 2}]
+        }"#;
+        let RequestPayload {
+            wasm_hex,
+            function_name,
+            params,
+        } = serde_json::from_str(req_payload).unwrap();
+        assert_eq!(
+            wasm_hex,
+            "0061736d0100000001060160017f017f030201000707010372756e00000a0601040020000b"
+        );
+        assert_eq!(function_name, "run");
+        assert_eq!(params, [WasmValue::I32(2)]);
+    }
 }
